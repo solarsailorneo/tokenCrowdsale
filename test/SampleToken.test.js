632dfbc0fa27c3008b382dd4f8ea13dc0f26583f
Solar Sailor <solarsailorneo@protonmail.com>
2022-06-10 14:38:59 -0400

    Added unit test to check all tokens are in my account

---
+++ test/SampleToken.test.js
@@ -0,0 +1,21 @@
+const Token = artifacts.require("SampleToken");
+
+var chai = require("chai");
+const BN = web3.utils.BN;
+const chaiBN = require("chai-bn")(BN);
+chai.use(chaiBN);
+
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
+
+contract("Token Test", async (accounts) => {
+    it("All tokens should be in my account", async () => {
+        let instance = await Token.deployed();
+        let totalSupply = await instance.totalSupply();
+        // let balance = await instance.balanceOf(accounts[0]);
+        // transformAsserterArgs.equal(balance.valueOf(), initialSupply.valueOf(), "The balance is not the same");
+        expect(await instance.balanceOf(accounts[0])).to.be.a.bignumber.equal(totalSupply);
+    })
+});
