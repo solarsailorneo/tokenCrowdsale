3dfe43a9d94b1cfa16a665265a1cdaf44b57a078
Solar Sailor <solarsailorneo@protonmail.com>
2022-06-11 12:20:09 -0400

    Coded crowdsale contract but still need to code tests

---
+++ contracts/SampleTokenSale.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./Crowdsale.sol";
+
+contract SampleTokenSale is Crowdsale {
+    constructor(
+        uint256 rate,    // rate in TKNbits
+        address payable wallet,
+        IERC20 token
+    )
+        Crowdsale(rate, wallet, token)
+        public
+    {
+
+    }
+}
